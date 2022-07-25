d9521ee292724ce0e952f13fc5317e7b671b835b
Yusuf Yigit <39989409+yusufyi@users.noreply.github.com>
2022-07-25 16:52:48 +0100

    done

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,13 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { apiSlice } from '../features/songs/song-api-slice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(apiSlice.middleware);
   },
 });
 
